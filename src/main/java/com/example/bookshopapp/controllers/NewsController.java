e480df129a11ad596a4376f7cd0b0a504b2a00aa
a.stolnikov <a.stolnikov@draewil.com>
2021-01-28 17:11:31 +0300

    Initial commit

---
+++ src/main/java/com/example/bookshopapp/controllers/NewsController.java
@@ -0,0 +1,32 @@
+package com.example.bookshopapp.controllers;
+
+import com.example.bookshopapp.data.Author;
+import com.example.bookshopapp.data.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class NewsController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public NewsController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @ModelAttribute("authorsMap")
+    public Map<String,List<Author>> authorsMap(){
+        return authorService.getAuthorsMap();
+    }
+
+    @GetMapping("/news")
+    public String authorsPage(){
+        return "/books/recent";
+    }
+}
